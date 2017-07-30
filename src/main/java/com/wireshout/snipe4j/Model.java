bdaa0477014f1e7330d27eb6b6544ca14c21bc0c
Jason Spriggs <jason@jasonspriggs.com>
2017-07-30 11:29:40 -0400

    Impl model

--- src/main/java/com/wireshout/snipe4j/Model.java
+++ src/main/java/com/wireshout/snipe4j/Model.java
@@ -1,15 +1,42 @@
 package com.wireshout.snipe4j;
 
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+
 public class Model extends SnipeObject {
 	private String model_number;
 	private Category category; //This returns a eula_text field, which might come in handy for the Category bit. use_default_eula, checkin_email, and require_acceptance are not booleans
-	private Manufacturer manufacturer; //manufacturer_id
-	private Depreciation depreciation; //TODO: this is REALLY horrible. It returns "No" when not set, and when it is, it returns the object, but also with a "user_id" parameter???
+	private Manufacturer manufacturer;
+	private Depreciation depreciation;
 	private String image;
 	private int assets_count;
 	private FieldSet fieldset;
 	private String notes;
 	private int eol; //TODO: This seems really horrible. Is returned when none is set as "None", or "n months" when it is set
+
+	public Model(SnipeInstance snipe, int id) {
+		super(snipe, id, "models");
+		HashMap<String, Object> detail = refresh();
+		if(detail.get("manufacturer") != null) {
+			JSONObject obj =  (JSONObject) detail.get("manufacturer");
+			Long tmpid = (Long) obj.get("id");
+			manufacturer = new Manufacturer(snipe, tmpid.intValue());
+		}
+		if(detail.get("category") != null) {
+			JSONObject obj =  (JSONObject) detail.get("category");
+			Long tmpid = (Long) obj.get("id");
+			category = new Category(snipe, tmpid.intValue());
+		}
+		if(detail.get("depreciation") != null) {
+			JSONObject obj =  (JSONObject) detail.get("depreciation");
+			Long tmpid = (Long) obj.get("id");
+			depreciation = new Depreciation(snipe, tmpid.intValue());
+		}
+		//TODO: impl model_number, notes, image, assets_count, fieldset, eol
+	}
 	
-	//We need to have created_at and updated_at times reported
+	public Model(SnipeInstance snipe, ModelFactory create) {
+		super(snipe, create);
+	}
 }
